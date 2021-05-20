e7659852fa8dc729c30a93655f66dc3641817404
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-05-20 10:20:57 +0000

    api init

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qz)hjb!!*)4!114qp(k=5l832v9d&l5%-h!)@nnxp-_tyff&86'
-
+with open(BASE_DIR/'.secret_key','r') as f:
+    SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
 
 
 # Application definition
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
